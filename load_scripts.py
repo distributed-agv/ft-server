08e52edb9bec661c79a739af6e1cfb142e57bd2e
billsjchw <billsjc@126.com>
2021-01-13 22:45:00 -0500

    feat: load_scripts.py loads locator_recover.lua

--- load_scripts.py
+++ load_scripts.py
@@ -6,7 +6,9 @@ if __name__ == '__main__':
     config = json.load(open('config.json', 'r'))
     commit_script = open('commit.lua', 'r').read()
     recover_script = open('recover.lua', 'r').read()
+    locator_recover_script = open('car-locator/locator_recover.lua', 'r').read()
 
     r = redis.Redis(config['redis_addr']['host'], config['redis_addr']['port'])
     r.script_load(commit_script)
     r.script_load(recover_script)
+    r.script_load(locator_recover_script)
